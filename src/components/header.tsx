cd26c6d7759cc583c5e0b2648b51dad327b35e3e
Alex Christakou <alexchristakou@gmail.com>
2024-04-30 15:07:34 +0100

    created basic navbar, installed shadcn

---
+++ src/components/header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <div className="flex justify-between px-4 pt-4">
+            <h1>macrovue</h1>
+            <h2>United States</h2>
+        </div>
+  )
+}
---
+++ src/components/sidebar.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+interface SidebarProps{
+    children: React.ReactNode;
+}
+
+export default function Sidebar({ children } : SidebarProps) {
+  return (
+      <div className="flex">
+          <div className="fixed w-20 h-screen p-4 bg-white border-r-[1px] flex flex-col justify-between">
+              <div className="flex flex-col items-center">
+                  <Link href="/">
+                      Hello, this is a test 
+                  </Link>
+              </div> 
+          </div>
+          <main className="ml-20 w-full">
+              {children}
+          </main>
+    </div>
+  )
+}
